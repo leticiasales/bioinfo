ae40dfab02546a47cf2c635bef89f8c7849baf0c
leticia <letisaless@gmail.com>
2019-05-29 14:54:41 -0300

    still has a bug
    Signed-off-by: leticia <letisaless@gmail.com>

--- run.py
+++ run.py
@@ -6,43 +6,46 @@ def frag(seq):
 	#faz uma quebra aleatória do DNA
 	#resultando em várias cópias quebradas 
 	#em posições aleatórias de suas sequências
-	return ['abc', 'cdef', 'fg', 'fghi']
+	return ['abc', 'cdef', 'fg', 'fghi', 'da']
 
 def sym_val_2(a, b):
 	#procurar pela existência de casamento 
 	#entre o sufixo de a e o prefixo de b
 	sym = []
-	print('sym_val_2(' + a + ', ' + b + ')')
 	for i in range(len(b)):
 		aux = a.find(b[i:])
 		if aux != -1:
-			sym.append(aux)
+			size = len(b[i:])
+			sym.append(-1 * size)
 		aux = a.find(b[:i])
 		if aux != -1:
-			sym.append(aux)
-	# print(sym)
-	return(max(sym) if len(sym) else 0)
+			size = len(b[:i])
+			sym.append(size)
+	return(max(sym, key=abs) if len(sym) else 0)
 
 def sym_val_full(a, b):
 	#escolhe máximo entra sym_val_2 para
 	#a e b e sym_val_2 para b e a
-  # print(a, b)
-  c = sym_val_2(a, b)
-  d = sym_val_2(b, a)
-  if c == -1: return d
-  if d == -1: return c
-  # print (c, d)
-  return c if (abs(c) > abs(d)) else d
+  return sym_val_2(a, b)
 
 def find_relevant(frags):
 	#para todo par (a, b) chama sym_val_full(a, b)
 	#se o retorno for maior que min_val
 	#e diferente de 100%
 	#adiciona a e b no valor de retorno
+	sym=[];orig=[];dest=[];
 	for x in range(len(frags) - 1):
 		for y in range(len(frags) - 1):
-			print(sym_val_full(frags[x], frags[y + 1]))
-	return(frags, frags)
+			if x != (y + 1): 
+				aux = sym_val_full(frags[x], frags[y + 1])
+				# exit()
+				print(frags[x], frags[y + 1])
+				print(aux)
+				if abs(aux) > min_val:
+					sym.append(aux)
+					orig.append(frags[x])
+					dest.append(frags[y + 1])
+	return(sym, orig, dest)
 
 
 def main():
@@ -53,9 +56,11 @@ def main():
 	frags = frag(seq)
 	
 	#encontra relevantes
-	contigs, sym_val = find_relevant(frags)
+	sym, orig, dest = find_relevant(frags)
 
-	print(contigs)
+	print(sym)
+	print(orig)
+	print(dest)
 
 	#para todos os contigs
 		#add contigs no grafo G como vértice 
