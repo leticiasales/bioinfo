c73dc990649cc61b3dab10a1be82596de07b3576
leticia <letisaless@gmail.com>
2019-06-04 16:29:20 -0300

    using libraries
    Signed-off-by: leticia <letisaless@gmail.com>

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 # coding=utf-8
 
+import networkx as nx
+import difflib
+import matplotlib.pyplot as plt
+
 min_val = 0
 
 def frag(seq):
@@ -8,25 +12,17 @@ def frag(seq):
 	#em posições aleatórias de suas sequências
 	return ['abc', 'cdef', 'fg', 'fghi', 'da']
 
-def sym_val_2(a, b):
-	#procurar pela existência de casamento 
-	#entre o sufixo de a e o prefixo de b
-	sym = []
-	for i in range(len(b)):
-		aux = a.find(b[i:])
-		if aux != -1:
-			size = len(b[i:])
-			sym.append(-1 * size)
-		aux = a.find(b[:i])
-		if aux != -1:
-			size = len(b[:i])
-			sym.append(size)
-	return(max(sym, key=abs) if len(sym) else 0)
+def get_overlap(s1, s2):
+    s = difflib.SequenceMatcher(None, s1, s2)
+    pos_a, pos_b, size = s.find_longest_match(0, len(s1), 0, len(s2)) 
+    return size
 
 def sym_val_full(a, b):
 	#escolhe máximo entra sym_val_2 para
 	#a e b e sym_val_2 para b e a
-  return sym_val_2(a, b)
+  # print(get_overlap(a,b))
+  # print(get_overlap(b,a))
+  return get_overlap(a, b)
 
 def find_relevant(frags):
 	#para todo par (a, b) chama sym_val_full(a, b)
@@ -38,9 +34,6 @@ def find_relevant(frags):
 		for y in range(len(frags) - 1):
 			if x != (y + 1): 
 				aux = sym_val_full(frags[x], frags[y + 1])
-				# exit()
-				print(frags[x], frags[y + 1])
-				print(aux)
 				if abs(aux) > min_val:
 					sym.append(aux)
 					orig.append(frags[x])
@@ -58,15 +51,30 @@ def main():
 	#encontra relevantes
 	sym, orig, dest = find_relevant(frags)
 
-	print(sym)
-	print(orig)
-	print(dest)
+	# print(sym)
+	# print(orig)
+	# print(dest)
+
+	# create graph g
+	G = nx.MultiDiGraph()
+	plt.subplot(122)
+
+	for i in range(len(sym)):
+		G.add_weighted_edges_from([(orig[i], dest[i], sym[i])])
+		#g_o = find_or_create(graph g, orig[i])
+		#g_d = find_or_create(graph g, dest[i])
+		#create_edge(from: g_o, to: g_d, peso: sym[i])
+
+	for (u, v, wt) in G.edges.data('weight'):
+		print('(%s, %s, %d)' % (u, v, wt))
+
+
+	nx.draw(G, with_labels=True, font_weight='bold')
+	plt.show()
 
 	#para todos os contigs
 		#add contigs no grafo G como vértice 
 		#orientação das arestas é dada pela ordem do casamento
 		#peso da aresta é dado pelo sym_val
 
-	pass
-
-main()
+	# return find_longest_path(g)
