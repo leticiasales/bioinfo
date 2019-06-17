c565b386d39001e38cd77d9a81145333ce49597b
leticia <letisaless@gmail.com>
2019-06-17 15:25:55 -0300

    code cleanup
    Signed-off-by: leticia <letisaless@gmail.com>

--- mine.py
+++ mine.py
@@ -7,25 +7,26 @@ import matplotlib.pyplot as plt
 min_val = 0
 
 def frag(seq):
-	#faz uma quebra aleatória do DNA
-	#resultando em várias cópias quebradas 
-	#em posições aleatórias de suas sequências
+	#le arquivo in.txt e considera cada linha um fragmento
 	file = open('in.txt')
 	frags = []
 	for line in file:
 		for n in line.split("\n"):
 			frags.append(n)
-	# frags = ["abc", "gbf", "cefg"]
 	return frags
 
-def sym_val_2(a, b):
-	#procurar pela existência de casamento 
-	#entre o sufixo de a e o prefixo de b
+def sym_val(a, b):
+	# procura pela existência de casamento 
+	# entre o sufixo de a e o prefixo de b 
+	# e vice versa
 	sym = []
 	for i in range(len(b)):
 		aux = a.find(b[i:])
 		size = len(b[i:])
 		if aux != -1 and (aux == 0 or aux == len(a) - size):
+			# ignora quando estiver
+			# mais longe da borda do que é o tamanho 
+			# garantindo que não está no meio
 			sym.append(-1 * size)
 		aux = a.find(b[:i])
 		size = len(b[:i])
@@ -33,17 +34,8 @@ def sym_val_2(a, b):
 			sym.append(size)
 	return(max(sym, key=abs) if len(sym) else 0)
 
-# tamanho da substring encontrada é
-	# adicionar pra ignorar quando estiver
-	# mais longe da borda do que é o tamanho
-
-def sym_val_full(a, b):
-	#escolhe máximo entra sym_val_2 para
-	#a e b e sym_val_2 para b e a
-	return sym_val_2(a, b)
-
 def find_relevant(frags):
-	#para todo par (a, b) chama sym_val_full(a, b)
+	#para todo par (a, b) chama sym_val(a, b)
 	#se o retorno for maior que min_val
 	#e diferente de 100%
 	#adiciona a e b no valor de retorno
@@ -51,7 +43,7 @@ def find_relevant(frags):
 	for x in range(len(frags) - 1):
 		for y in range(len(frags) - 1):
 			if x != (y + 1): 
-				aux = sym_val_full(frags[x], frags[y + 1])
+				aux = sym_val(frags[x], frags[y + 1])
 				if aux >= len(frags[x]):
 					ign.append(frags[x])
 				if aux >= len(frags[y + 1]):
@@ -61,31 +53,25 @@ def find_relevant(frags):
 					orig.append(frags[x])
 					dest.append(frags[y + 1])
 	for item in ign:
+		# ignorar substrings contidas
 		try: 
 			while(orig.index(item)):
-				# print(orig)
 				index = orig.index(item)
 				del orig[index]
 				del dest[index]
 				del sym[index]
-				# print(orig)
 		except: pass
 		try:
 			while(dest.index(item)):
-				# print(dest)
 				index = dest.index(item)
 				del orig[index]
 				del dest[index]
 				del sym[index]
-				# print(dest)
 		except: pass
 	return(sym, orig, dest)
 
 def main():
-	#lê sequencia
-	seq = 'abcdefghi'
-
-	#fragmenta sequencia
+	#lê sequencia a partir de arquivo
 	frags = frag(seq)
 	
 	#encontra relevantes
@@ -98,30 +84,30 @@ def main():
 	# create graph g
 	G = nx.MultiDiGraph()
 
+	#para todos os contigs
+		#add contigs no grafo G como vértice 
+		#orientação das arestas é dada pela ordem do casamento
+		#peso da aresta é dado pelo sym_val
+
 	for i in range(len(sym)):
 		H = G.copy()
 		if(sym[i] < 0):
 			try:
 				H.add_weighted_edges_from([(dest[i], orig[i], abs(sym[i]))])
 				nx.find_cycle(H, orientation='original')
+				# teste se a adição da aresta cria um ciclo
 			except:
+				# se não, mantém a aresta
 				G = H.copy()
 		else:
+			# inverte origem e destino se negativo
 			try:
 				H.add_weighted_edges_from([(orig[i], dest[i], abs(sym[i]))])
 				nx.find_cycle(H, orientation='original')
+				# teste se a adição da aresta cria um ciclo
 			except:
 				G = H.copy()
-		#g_o = find_or_create(graph g, orig[i])
-		#g_d = find_or_create(graph g, dest[i])
-		#create_edge(from: g_o, to: g_d, peso: sym[i])
-
-	# for (u, v, wt) in G.edges.data('weight'):
-	# 	print('(%s, %s, %d)' % (u, v, wt))
-
-	# plt.subplot(121)
-	# nx.draw(G, with_labels=True, font_weight='bold')
-	# plt.show()
+				# se não, mantém a aresta
 
 	path = nx.dag_longest_path(G)
 
@@ -130,15 +116,10 @@ def main():
 	gem = ""
 
 	for i in range(len(path) - 2):
-		gem += path[i][:sym_val_full(path[i], path[i+1])]
+		gem += path[i][:sym_val(path[i], path[i+1])]
 
 	gem += path[len(path) - 1]
 
 	print(gem)
-	#para todos os contigs
-		#add contigs no grafo G como vértice 
-		#orientação das arestas é dada pela ordem do casamento
-		#peso da aresta é dado pelo sym_val
 
-	# return find_longest_path(g)
 main()
